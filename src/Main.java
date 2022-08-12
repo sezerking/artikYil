9ff71ef22fa27e26c507288412d1db23d6b0e37d
sezerking <sezerking123@gmail.com>
2022-08-13 01:50:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+/*Artık Yıl Nasıl Hesaplanır?
+
+Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:
+
+1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.
+100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+
+Örneğin 1200, 1600, 2000 yılları artık yıldır ancak 1700, 1800 ve 1900 artık yıl değildir.
+Sadece 400'e tam olarak bölünebilenlerin artık yıl kabul edilmesinin nedeni, bir astronomik yılın 365,25 gün değil, yaklaşık olarak 365,242 gün olmasından kaynaklanan hatayı gidermektir.
+*/
+public class Main {
+    public static void main(String[] args) {
+        int yil;
+        boolean artikYil;
+        Scanner scn=new Scanner(System.in);
+
+        System.out.println("Yıl giriniz: ");
+        yil=scn.nextInt();
+
+        if (yil%4==0){
+            if (yil%100==0){
+                if (yil%400==0){
+                    artikYil=true;
+                }
+                else {
+                    artikYil=false;
+                }
+            }
+            else{
+                artikYil=true;
+            }
+
+        }
+        else {
+            artikYil=false;
+        }
+
+        if (artikYil){
+            System.out.println(yil+" bir artık yıldır!");
+        }
+        else{
+            System.out.println(yil+" bir artık yıl değildir.");
+        }
+
+    }
+}
